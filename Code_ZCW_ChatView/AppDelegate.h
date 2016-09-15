25b095ada5ca617583c1c73492afcf0b45e874fd
zcwlwen <zcw_lwen@sina.cn>
2016-09-15 15:16:26 +0800

    Initial Commit

---
+++ Code_ZCW_ChatView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Code_ZCW_ChatView
+//
+//  Created by 张朝伟 on 16/9/15.
+//  Copyright © 2016年 张朝伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code_ZCW_ChatView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Code_ZCW_ChatView
+//
+//  Created by 张朝伟 on 16/9/15.
+//  Copyright © 2016年 张朝伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
